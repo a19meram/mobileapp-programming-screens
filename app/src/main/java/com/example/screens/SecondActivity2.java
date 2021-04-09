83604755292b2a37c8f990d067a91906513e1e93
a19meram <a19meram@student.his.se>
2021-04-09 11:41:32 +0200

    Added a new Activity!

---
+++ app/src/main/java/com/example/screens/SecondActivity2.java
@@ -0,0 +1,15 @@
+package com.example.screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second2);
+
+    }
+}
