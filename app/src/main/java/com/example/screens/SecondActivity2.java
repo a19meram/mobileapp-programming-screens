7986c95f6686f2fd7903776c14a14c90229f1ff5
a19meram <a19meram@student.his.se>
2021-04-09 12:10:48 +0200

    Added a button to open the SecondActivity and to close the SecondActivity!

--- app/src/main/java/com/example/screens/SecondActivity2.java
+++ app/src/main/java/com/example/screens/SecondActivity2.java
@@ -3,6 +3,9 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity2 extends AppCompatActivity {
 
@@ -10,6 +13,23 @@ public class SecondActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second2);
+
+        TextView textView = findViewById(R.id.name);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String name = extras.getString("name");
+            textView.setText(name);
+        }
+
+        Button close = findViewById(R.id.My_button2);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
 
     }
 }
